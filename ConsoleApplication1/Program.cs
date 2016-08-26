c0c74af5a5c96d5196ecdca1954f3c5b61325230
Chen <jchen@laterooms.com>
2016-08-26 15:44:53 +0100

    Buillt Stock class.

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -13,4 +13,53 @@ namespace ConsoleApplication1
 
         }
     }
+
+    public class Stock
+    {
+        private double _price;
+        private string _symbol;
+        private List<IInvestor> _investors;
+
+        public Stock(string symbol, double price)
+        {
+            _symbol = symbol;
+            _price = price;
+        }
+
+        public void AttachInvestor(IInvestor investor)
+        {
+            _investors.Add(investor);
+        }
+
+        public void Notify()
+        {
+            foreach (var investor in _investors)
+            {
+                investor.Update();
+            }
+        }
+
+        public double Price
+        {
+            set
+            {
+                if (_price != value)
+                {
+                    _price = value;
+                    Notify();
+                }
+            }
+            get { return _price; }
+        }
+
+        public string Symbol
+        {
+            get { return _symbol; }
+        }
+    }
+    
+    public interface IInvestor
+    {
+        void Update();
+    }
 }
